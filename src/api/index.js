09fd4e1e5d14a17a08497a467acdcfc9beeb8891
Bhavesh Daipuria <bdaipuria@gmail.com>
2025-09-19 01:58:53 +0530

    fix: change base url for server

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://stackoverflow-clone-server-gds6.onrender.com",
+  baseURL: "https://stackoverflow-clone-server-neon.vercel.app",
 });
 
 API.interceptors.request.use((req) => {
